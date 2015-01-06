5fa19a8cb8bf43adaecf1a5be00ae9d9bae2532f
GregorySchwartz <gregory.schwartz@drexel.edu>
2015-01-06 00:24:43 -0500

    Updated colors

--- scripts/get_color_scheme.sh
+++ scripts/get_color_scheme.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 file=$(gsettings get org.gnome.desktop.background picture-uri | sed -e "s/file:\/\///" -e "s/'//g" -e "s/\n//")
 
 schemer -t=50 -term="urxvt" ${file} > ~/.colors/dynamic_background.txt
