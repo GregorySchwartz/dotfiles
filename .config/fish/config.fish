8adc92abdcca886b78edf264615d3a26954272c6
GregorySchwartz <gregory.schwartz@drexel.edu>
2015-04-08 14:48:11 -0400

    New alias for fish session

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,11 @@ set PATH /home/gw/haskell/yesod-bin/.cabal-sandbox/bin/ $PATH
 # Truecolor in nvim
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
 
-# base16 colors in terminal
-eval sh /home/gw/.config/base16-shell/base16-wasteland.dark.sh
-
 # Prevent flickering in libreoffice in xmonad
 set -x SAL_USE_VCLPLUGIN gen
+
+# base16 colors in terminal
+eval sh /home/gw/.config/base16-shell/base16-eighties.dark.sh
+
+# Aliases
+alias sesh "dtach -A /tmp/gw -r winch nvim"
