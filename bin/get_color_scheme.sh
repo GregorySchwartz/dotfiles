c575351c124f1492ba00ffd2861cd286befe5a28
GregorySchwartz <gregory.schwartz@drexel.edu>
2015-05-04 12:56:03 -0400

    Changed bin folder

---
+++ bin/get_color_scheme.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+file=$(gsettings get org.gnome.desktop.background picture-uri | sed -e "s/file:\/\///" -e "s/'//g" -e "s/\n//")
+
+schemer -t=50 -term="urxvt" ${file} > ~/.colors/dynamic_background.txt
+
+xrdb -merge ~/.Xresources
