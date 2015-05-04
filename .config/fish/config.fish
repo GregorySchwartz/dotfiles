f0f40a59b00a82cfdad35bc4845f1aa6b46741b4
GregorySchwartz <gregory.schwartz@drexel.edu>
2015-05-04 12:55:28 -0400

    New tvim command for use within nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,16 @@ set PATH /home/gw/haskell/yesod-bin/.cabal-sandbox/bin/ $PATH
 # Prevent flickering in libreoffice in xmonad
 set -x SAL_USE_VCLPLUGIN gen
 
-# Set editor
-set -x EDITOR nvim
-
 # base16 colors in terminal
 eval sh /home/gw/.config/base16-shell/base16-eighties.dark.sh
 
+####################
 # Aliases
-alias sesh "dtach -A /tmp/gw -r winch nvim"
+####################
+
+# Use tvim within nvim to open up a file in the host nvim instance from a nested
+# terminal
+alias tvim "python2 /home/gw/git_repos/dotfiles/bin/tvim.py"
+
+# For sessions
+alias sesh "dtach -A /tmp/gw -r winch tvim"
--- scripts/swm_boot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sxhkd -c /home/gw/.config/sxhkd/sxhkdrc_swm &
-
-xrdb -load ~/.Xresources
-
-exec /usr/bin/swm
