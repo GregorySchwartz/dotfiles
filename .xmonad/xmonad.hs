b64a62de425102275e949e458a5808062ad27b47
Gregory W. Schwartz <ifthenelse2244@gmail.com>
2019-03-20 16:17:42 -0400

    Automatic resolution for xmonad.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,8 @@ import Data.Monoid
 
 -- Cabal
 import DBus.Client
+import Graphics.X11 (openDisplay)
+import Graphics.X11.Xinerama (getScreenInfo)
 import XMonad
 import XMonad.Actions.CopyWindow
 import XMonad.Actions.GridSelect
@@ -22,66 +24,69 @@ import XMonad.Util.Run
 import qualified XMonad.StackSet as W
 import qualified XMonad.Util.ExtensibleState as XS
 
-data Resolution = HD | UHD
+type Height = Int
+
 data Device     = Desktop | Laptop
 
 -- | Be sure to choose the correct option: HD vs. UHD and Desktop vs.
 -- Laptop
 main :: IO ()
 main = do
-    xmonad . myConfig UHD $ Laptop
+    height <- fmap (fromIntegral . rect_height . head)
+            $ openDisplay [] >>= getScreenInfo :: IO Int
+    xmonad . myConfig height $ Desktop
 
-myConfig res dev = desktopConfig
+myConfig height dev = desktopConfig
     { modMask            = mod4Mask
     , terminal           = "kitty"
-    , borderWidth        = borderRes res
+    , borderWidth        = borderRes height
     , workspaces         = myWorkspaces
     , manageHook         = namedScratchpadManageHook scratchpads
                        <+> manageHook desktopConfig
-    , layoutHook         = smartBorders . myLayout $ res
+    , layoutHook         = smartBorders . myLayout $ height
     , handleEventHook    = handleEventHook def
     , normalBorderColor  = colors "black"
     , focusedBorderColor = colors "darkred"
-    } `additionalKeysP` myKeys res dev
+    } `additionalKeysP` myKeys height dev
 
 -- | Define the border width
-borderRes :: Resolution -> Dimension
-borderRes HD  = 5
-borderRes UHD = 10
+borderRes :: Height -> Dimension
+borderRes = fromIntegral . round . (/ 200) . fromIntegral
 
 -- My shortcuts. Also changes greedyView to view for multiple monitors
-myKeys :: Resolution -> Device -> [(String, X ())]
-myKeys res dev = [ ("M4-p", spawn . rofiRunCommand $ res) -- open program
-                 , ("M4-o", spawn . rofiWindowCommand $ res) -- switch window
-                 , ("M4-z", sendMessage MirrorShrink) -- lower bottom focused right column
-                 , ("M4-a", sendMessage MirrorExpand) -- raise bottom focused right column
-                 , ("M4-C-l", spawn "xscreensaver-command --lock") -- to lock
-                 , ("M4-C-<End>", spawn "amixer -q sset Capture toggle") -- toggle mute mic
-                 , ("M4-r", restart "xmonad" True) -- to restart without recompile
-                 , ("M4-g", goToSelected . myGSConfig $ res) -- grid select
-                 , ("M4-x", spawn "xkill") -- kill program with mouse
-                 , ("M4-C-e", namedScratchpadAction scratchpads "editor") --scratchpad
-                 , ("M4-C-m", namedScratchpadAction scratchpads "music") --scratchpad
-                 , ("M4-C-s", namedScratchpadAction scratchpads "slack") --scratchpad
-                 , ("M4-C-k", namedScratchpadAction scratchpads "keepass") --scratchpad
-                 , ("M4-c", placeFocused . fixed $ (0.5, 0.5)) -- center window
-                 , ("M4-v", windows copyToAll) -- Make focused window always visible
-                 , ("M4-S-v", killAllOtherCopies) -- Toggle window state back
-                 , ("M4-C-c", spawn "killall compton || compton --config ~/.config/compton.conf &") -- toggle compositor
-                 , ("C-<Home>", spawn "playerctl play-pause") -- mpd toggle play pause
-                 , ("C-<End>", spawn "playerctl stop") -- mpd stop
-                 , ("C-<Page_Up>", spawn "playerctl previous") -- mpd previous
-                 , ("C-<Page_Down>", spawn "playerctl next") -- mpd next
-                 , ("C-S-1", spawn "setxkbmap -v us") -- qwerty
-                 , ("C-S-2", spawn "setxkbmap -v us -variant colemak") -- colemak
-                 ]
-              ++ [ (otherModMasks ++ "M-" ++ [key], action tag)
-                 | (tag, key)  <- zip myWorkspaces "123456789"
-                 , (otherModMasks, action) <- [ ("", windows . W.view) -- was W.greedyView
-                                              , ("S-", windows . W.shift)
-                                              ]
-                 ]
-              ++ resolutionKeys dev
+myKeys :: Height -> Device -> [(String, X ())]
+myKeys height dev =
+    [ ("M4-p", spawn . rofiRunCommand $ height) -- open program
+    , ("M4-o", spawn . rofiWindowCommand $ height) -- switch window
+    , ("M4-z", sendMessage MirrorShrink) -- lower bottom focused right column
+    , ("M4-a", sendMessage MirrorExpand) -- raise bottom focused right column
+    , ("M4-C-l", spawn "xscreensaver-command --lock") -- to lock
+    , ("M4-C-<End>", spawn "amixer -q sset Capture toggle") -- toggle mute mic
+    , ("M4-r", restart "xmonad" True) -- to restart without recompile
+    , ("M4-g", goToSelected . myGSConfig $ height) -- grid select
+    , ("M4-x", spawn "xkill") -- kill program with mouse
+    , ("M4-C-e", namedScratchpadAction scratchpads "editor") --scratchpad
+    , ("M4-C-m", namedScratchpadAction scratchpads "music") --scratchpad
+    , ("M4-C-s", namedScratchpadAction scratchpads "slack") --scratchpad
+    , ("M4-C-k", namedScratchpadAction scratchpads "keepass") --scratchpad
+    , ("M4-c", placeFocused . fixed $ (0.5, 0.5)) -- center window
+    , ("M4-v", windows copyToAll) -- Make focused window always visible
+    , ("M4-S-v", killAllOtherCopies) -- Toggle window state back
+    , ("M4-C-c", spawn "killall compton || compton --config ~/.config/compton.conf &") -- toggle compositor
+    , ("C-<Home>", spawn "playerctl play-pause") -- mpd toggle play pause
+    , ("C-<End>", spawn "playerctl stop") -- mpd stop
+    , ("C-<Page_Up>", spawn "playerctl previous") -- mpd previous
+    , ("C-<Page_Down>", spawn "playerctl next") -- mpd next
+    , ("C-S-1", spawn "setxkbmap -v us") -- qwerty
+    , ("C-S-2", spawn "setxkbmap -v us -variant colemak") -- colemak
+    ]
+ ++ [ (otherModMasks ++ "M-" ++ [key], action tag)
+    | (tag, key)  <- zip myWorkspaces "123456789"
+    , (otherModMasks, action) <- [ ("", windows . W.view) -- was W.greedyView
+                                 , ("S-", windows . W.shift)
+                                 ]
+    ]
+  ++ resolutionKeys dev
 
 -- | Assign keys based on laptop UHD or HD
 resolutionKeys :: Device -> [(String, X ())]
@@ -102,11 +107,11 @@ resolutionKeys Laptop  =
 myWorkspaces :: [String]
 myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
 
-myLayout res = (avoidStruts . smartSpacingWithEdge (space res) $ tiled) ||| Full
+myLayout height = (avoidStruts . smartSpacingWithEdge (space height) $ tiled)
+              ||| Full
   where
     -- Space between windows
-    space HD  = 10
-    space UHD = 20
+    space = fromIntegral . round . (/ 100) . fromIntegral
     -- default tiling algorithm partitions the screen into two panes
     tiled = ResizableTall nmaster delta ratio []
 
@@ -128,58 +133,53 @@ scratchpads = [ NS "editor" "emacsclient -c -a \"\" -F '((name  . \"Emacs Scratc
   where
     scratchFloat = customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3)
 
-myGSConfig HD  = def { gs_font = "xft:Open Sans Light-14"
-                     , gs_cellheight = 100
-                     , gs_cellwidth = 300
-                     }
-myGSConfig UHD = def { gs_font = "xft:Open Sans Light-14"
-                     , gs_cellheight = 200
-                     , gs_cellwidth = 600
-                     }
+myGSConfig height = def { gs_font = "xft:Open Sans Light-14"
+                        , gs_cellheight = size
+                        , gs_cellwidth = size * 3
+                        }
+  where
+    size = fromIntegral . round . (/ 10) . fromIntegral $ height
 
-tabbedTheme :: Resolution -> Theme
-tabbedTheme res = def { activeColor         = colors "darkred"
-                      , activeBorderColor   = colors "red"
-                      , activeTextColor     = colors "white"
-                      , inactiveColor       = colors "black"
-                      , inactiveBorderColor = colors "grey"
-                      , inactiveTextColor   = colors "white"
-                      , urgentColor         = colors "darkmagenta"
-                      , urgentBorderColor   = colors "magenta"
-                      , urgentTextColor     = colors "white"
-                      , fontName            = "xft:Open Sans-11"
-                      , decoHeight          = fromIntegral . barSize $ res
-                      }
+tabbedTheme :: Height -> Theme
+tabbedTheme height = def { activeColor         = colors "darkred"
+                         , activeBorderColor   = colors "red"
+                         , activeTextColor     = colors "white"
+                         , inactiveColor       = colors "black"
+                         , inactiveBorderColor = colors "grey"
+                         , inactiveTextColor   = colors "white"
+                         , urgentColor         = colors "darkmagenta"
+                         , urgentBorderColor   = colors "magenta"
+                         , urgentTextColor     = colors "white"
+                         , fontName            = "xft:Open Sans-11"
+                         , decoHeight          = fromIntegral . barSize $ height
+                         }
 
 -- | Size of the bar
-barSize :: Resolution -> Int
-barSize HD  = 25
-barSize UHD = 55
+barSize :: Height -> Int
+barSize = round . (/ 40) . fromIntegral
 
 -- | rofi run command. Goes in this order: "bg,fg,bgalt,hlbg,hlfg"
-rofiRunCommand :: Resolution -> String
-rofiRunCommand res = "rofi -show run -font '" ++ font res ++ "' "
-                  ++ "-color-normal '"
-                  ++ intercalate "," [ colors "background"
-                                     , colors "foreground"
-                                     , colors "background"
-                                     , colors "darkblue"
-                                     , colors "foreground"
-                                     ]
-                  ++ "' -color-window '"
-                  ++ intercalate "," [ colors "background"
-                                     , colors "background"
-                                     ]
-                  ++ "' -fuzzy -bw 0 -separator-style solid -fullscreen -padding " ++ padding res ++ " -eh 2 -opacity 90 -lines 6 -hide-scrollbar"
+rofiRunCommand :: Height -> String
+rofiRunCommand height = "rofi -show run -font '" ++ font height ++ "' "
+                     ++ "-color-normal '"
+                     ++ intercalate "," [ colors "background"
+                                        , colors "foreground"
+                                        , colors "background"
+                                        , colors "darkblue"
+                                        , colors "foreground"
+                                        ]
+                     ++ "' -color-window '"
+                     ++ intercalate "," [ colors "background"
+                                        , colors "background"
+                                        ]
+                     ++ "' -fuzzy -bw 0 -separator-style solid -fullscreen -padding " ++ padding height ++ " -eh 2 -opacity 90 -lines 6 -hide-scrollbar"
   where
-    font HD     = "Open Sans Light 25"
-    font UHD    = "Open Sans Light 40"
-    padding HD  = "200"
-    padding UHD = "400"
+    font = ("Open Sans Light " ++) . show . round . (/ 40) . fromIntegral
+    padding = show . round . (/ 5) . fromIntegral
 
 -- | rofi window command. Goes in this order: "bg,fg,bgalt,hlbg,hlfg"
-rofiWindowCommand :: Resolution -> String
-rofiWindowCommand res = "rofi -show window -font '" ++ font res ++ "' "
+rofiWindowCommand :: Height -> String
+rofiWindowCommand height = "rofi -show window -font '" ++ font height ++ "' "
                      ++ "-color-normal '"
                      ++ intercalate "," [ colors "background"
                                         , colors "foreground"
@@ -198,12 +198,10 @@ rofiWindowCommand res = "rofi -show window -font '" ++ font res ++ "' "
                      ++ intercalate "," [ colors "background"
                                         , colors "background"
                                         ]
-                     ++ "' -fuzzy -bw 0 -separator-style solid -bc '#282828' -fullscreen -padding " ++ padding res ++ " -eh 2 -opacity 90 -lines 6 -hide-scrollbar"
+                     ++ "' -fuzzy -bw 0 -separator-style solid -bc '#282828' -fullscreen -padding " ++ padding height ++ " -eh 2 -opacity 90 -lines 6 -hide-scrollbar"
   where
-    font HD     = "Open Sans Light 25"
-    font UHD    = "Open Sans Light 40"
-    padding HD  = "200"
-    padding UHD = "400"
+    font = ("Open Sans Light " ++) . show . round . (/ 40) . fromIntegral
+    padding = show . round . (/ 5) . fromIntegral
 
 -- | Colors for everything
 colors :: String -> String
