040640b7f4088e438b2959d8893e91f98213efb2
GregorySchwartz <gregory.schwartz@uhn.ca>
2025-03-19 14:25:53 -0400

    Niri working.

--- .config/niri/work_open.sh
+++ .config/niri/work_open.sh
@@ -1,17 +1,29 @@
-niri msg action spawn "emacsclient -c -F '((name . \"Emacs Scratchpad\"))'"
-niri msg action move-window-to-workspace "Writing"
+niri msg action spawn -- "davmail" "/home/gw/.davmailuhn.properties"
+niri msg action spawn -- "davmail" "/home/gw/.davmailupenn.properties"
+niri msg action spawn -- "emacs"
+niri msg action spawn -- "zotero"
+niri msg action spawn -- "firefox"
+niri msg action spawn -- "dolphin"
+niri msg action spawn -- "vesktop"
+niri msg action spawn -- "slack"
+niri msg action spawn -- "thunderbird"
+niri msg action spawn -- "keepassxc"
+niri msg action spawn -- "youtube-music"
 
-niri msg action spawn youtube-music
-niri msg action move-window-to-workspace "Scratchpad"
+# niri msg action spawn "emacsclient -c -F '((name . \"Emacs Scratchpad\"))'"
+# niri msg action move-window-to-workspace "Writing"
 
-niri msg action spawn slack
-niri msg action move-window-to-workspace "Scratchpad"
+# niri msg action spawn youtube-music
+# niri msg action move-window-to-workspace "Scratchpad"
 
-niri msg action spawn "firefox --name discord -P discord --no-remote --new-window https://discord.com/channels/@me"
-niri msg action move-window-to-workspace "Web"
+# niri msg action spawn slack
+# niri msg action move-window-to-workspace "Scratchpad"
 
-niri msg action spawn "keepass"
-niri msg action move-window-to-workspace "Scratchpad"
+# niri msg action spawn "firefox --name discord -P discord --no-remote --new-window https://discord.com/channels/@me"
+# niri msg action move-window-to-workspace "Web"
 
-niri msg action spawn "thunderbird"
-niri msg action move-window-to-workspace "Scratchpad"
+# niri msg action spawn "keepassxc"
+# niri msg action move-window-to-workspace "Scratchpad"
+
+# niri msg action spawn "thunderbird"
+# niri msg action move-window-to-workspace "Scratchpad"
--- bin/toggle_program.sh
+++ bin/toggle_program.sh
@@ -1,1 +1,1 @@
-[ "pgrep -f $1 &> /dev/null ; echo $?" ] && pkill "$1" || bash -c "$1"
+[ "pgrep -f $1 &> /dev/null ; echo $?" ] && pkill "$1" || exec "$1"
